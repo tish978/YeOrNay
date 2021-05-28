40b14c2394b862e52858e408fbdbc99f0aa1e83e
tish978 <satish.bisa@gmail.com>
2021-05-27 22:37:51 -0400

    Intro Music added to home screen

--- YeOrNay/ViewController.swift
+++ YeOrNay/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        playMusic()
         // Do any additional setup after loading the view.
     }
 
+    func playMusic(){
+            if let player = player, player.isPlaying{
+                // stop playback
+            } else {
+                // setup player and play
+                let urlString = Bundle.main.path(forResource: "stretchMyHands", ofType: "mp3")
+                
+                do {
+                    try AVAudioSession.sharedInstance().setMode(.default)
+                    try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                    
+                    guard let urlString = urlString else{
+                        return
+                    }
+                    
+                    player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                    
+                    guard let player = player else{
+                        return
+                    }
+                    
+                    player.play()
+                
+                } catch {
+                    print("something went wrong")
+                }
+            }
+            
+            print("Attempting to animate stroke")
+        }
 
 }
 
