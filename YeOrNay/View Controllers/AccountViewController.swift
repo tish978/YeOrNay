dc92ff65058b92bfae8d2fe1283cf8c7b49d2325
tish978 <satish.bisa@gmail.com>
2021-07-03 16:51:55 -0400

    Log in button updates, Account create screen, account log in screen

--- YeOrNay/View Controllers/AccountViewController.swift
+++ YeOrNay/View Controllers/AccountViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class AccountViewController: UIViewController {
 
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    @IBOutlet weak var signUpBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpButtons()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpButtons(){
+        Utilities.styleFilledButton(loginBtn)
+        Utilities.styleHollowButton(signUpBtn)
     }
-    */
 
 }
